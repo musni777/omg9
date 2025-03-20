08149b1f8e5842eb4a7231396b9bc1c82192b958
musni777 <167499646+musni777@users.noreply.github.com>
2025-03-20 15:39:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,46 +6,52 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
+// Enable CORS to allow connections from any origin
+app.use(cors({
+    origin: "*", // This allows all origins, you can restrict to specific origins if needed
     methods: ["GET", "POST"],
-  },
+}));
+
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Same here, allowing connections from all origins
+        methods: ["GET", "POST"],
+    },
 });
 
 let waitingUser = null;
 
 io.on("connection", (socket) => {
-  console.log("New user connected:", socket.id);
+    console.log("New user connected:", socket.id);
 
-  if (waitingUser) {
-    socket.partner = waitingUser;
-    waitingUser.partner = socket;
+    if (waitingUser) {
+        socket.partner = waitingUser;
+        waitingUser.partner = socket;
 
-    socket.emit("chat-started");
-    waitingUser.emit("chat-started");
+        socket.emit("chat-started");
+        waitingUser.emit("chat-started");
 
-    waitingUser = null;
-  } else {
-    waitingUser = socket;
-    socket.emit("waiting");
-  }
-
-  socket.on("message", (msg) => {
-    if (socket.partner) {
-      socket.partner.emit("message", msg);
+        waitingUser = null;
+    } else {
+        waitingUser = socket;
+        socket.emit("waiting");
     }
-  });
 
-  socket.on("disconnect", () => {
-    if (socket.partner) {
-      socket.partner.emit("partner-disconnected");
-      socket.partner.partner = null;
-    }
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
-  });
+    socket.on("message", (msg) => {
+        if (socket.partner) {
+            socket.partner.emit("message", msg);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        if (socket.partner) {
+            socket.partner.emit("partner-disconnected");
+            socket.partner.partner = null;
+        }
+        if (waitingUser === socket) {
+            waitingUser = null;
+        }
+    });
 });
 
 server.listen(5000, () => console.log("Server running on port 5000"));
